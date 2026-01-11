5f6d80c30d9113ad33d1bd4cc0e4c0d739318e84
notKguin <kvitonovmaksim1@gmail.com>
2026-01-12 01:52:31 +0500

    Фикс отображения картинок

--- volunteer_service/urls.py
+++ volunteer_service/urls.py
@@ -1,14 +1,10 @@
-"""URL configuration for volunteer_service project."""
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
+    path("media/<path:path>", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
-
-# Media files for dev / docker runserver
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
